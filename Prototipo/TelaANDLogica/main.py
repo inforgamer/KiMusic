ef5f5376ce8de6e73b61fa3417552997dc1ef9e3
Hugo (Infor) Marcondes <143568062+inforgamer@users.noreply.github.com>
2026-08-03 20:23:23 -0300

    Criaça de classes

--- Prototipo/TelaANDLogica/main.py
+++ Prototipo/TelaANDLogica/main.py
@@ -4,34 +4,68 @@ from ili9341 import Display, color565
 from machine import Pin, SPI
 from rotary_irq_rp2 import RotaryIRQ  
 
-#Play/Pause
-botao_central = Pin(5,Pin.IN,Pin.PULL_UP)
-is_play = False
-is_pressed_play = False
 
-#Next
-botao_next = Pin(9,Pin.IN, Pin.PULL_UP)
-is_pressed_next = False
+class Encoder:
+  def __init__(self, min_val, max_val):
+      self.rotate = RotaryIRQ(pin_num_clk=2, 
+              pin_num_dt=3, 
+              min_val=min_val, 
+              max_val= max_val, 
+              reverse=False, 
+              range_mode=RotaryIRQ.RANGE_BOUNDED)
+      self.val_old = self.rotate.value()
 
-#Preview
-botao_preview = Pin(8,Pin.IN, Pin.PULL_UP)
-is_pressed_preview = False
+  def rotation(self) -> None:
+    if volume_button.do == True or position_button.do == True:
+      val_new = self.rotate.value()
+      if self.val_old != val_new:
+        self.val_old = val_new
+        print(val_new)
+        time.sleep(0.05)
 
-#Volume
-botao_volume = Pin(6,Pin.IN,Pin.PULL_UP)
-volume_mode = False
-is_pressed_volume = False
 
-#Posicao
-botao_posicao = Pin(7, Pin.IN,Pin.PULL_UP)
-position_mode = False
-is_pressed_position = False
+volume = Encoder(0, 100)
+class Button:
+    def __init__(self, pin_number):
+      self.pin_number = pin_number
+      self.pin = Pin(pin_number, Pin.IN, Pin.PULL_UP)
+      self.is_pressed = False
+      self.do = False
+
+
+    def check_click(self):
+      if self.pin.value() == 0 and self.is_pressed == False:
+        self.is_pressed = True
+        self.do = not self.do
+        
+        if self.pin_number == 5:
+            play(self.do)
+        elif self.pin_number == 8:
+              preview_music()
+        elif self.pin_number == 9:
+              next_music()
+        elif self.pin_number == 6:
+              set_volume(self.do)
+        elif self.pin_number == 7:
+              set_position(self.do)
+        time.sleep(0.1)
+      if self.pin.value() == 1 and self.is_pressed == True:
+        self.is_pressed = False
+        time.sleep(0.1)
+      
+      
+center_button = Button(5)
+preview_button = Button(8)
+next_button = Button(9)
+volume_button = Button(6)
+position_button = Button(7)
+
+
 
 #config de exibição
 spi =SPI(0, baudrate=40000000, sck=Pin(18), mosi=Pin(19))
 display = Display(spi, dc=Pin(14), cs=Pin(17), rst=Pin(15),
 width=320, height= 240, rotation= 270)
-
 
 #Play/Pause
 def play(is_play):
@@ -52,6 +86,8 @@ def preview_music():
 def set_volume(volume_mode):
   if volume_mode:
     print('Modo Volume')
+    
+    
   else:
     print('Modo Normal')
 
@@ -63,62 +99,14 @@ def set_position(position_mode):
       print("Modo normal")
   
 while True:
-  #Play/Pause
-  if botao_central.value() == 0 and is_pressed_play == False:
-     is_pressed_play = True
-     is_play = not is_play
-     play(is_play)
-     time.sleep(0.1)
-  if botao_central.value() == 1 and is_pressed_play == True:
-    is_pressed_play = False
-    time.sleep(0.1)
+  center_button.check_click()
+  preview_button.check_click()
+  next_button.check_click()
+  volume_button.check_click()
+  position_button.check_click()
 
-
-
-
-  #Next
-  if botao_next.value() == 0 and is_pressed_next == False:
-      is_pressed_next = True
-      next_music()
-      time.sleep(0.1)
-  if botao_next.value() == 1 and is_pressed_next == True:
-      is_pressed_next = False
-      time.sleep(0.1)
-
-
-
-  #Preview
-  if botao_preview.value() == 0 and is_pressed_preview == False:
-    is_pressed_preview = True
-    preview_music()
-    time.sleep(0.1)
-  if botao_preview.value() == 1 and is_pressed_preview == True:
-    is_pressed_preview = False
-    time.sleep(0.1)
-
-
-
-  #Volume
-  if botao_volume.value() == 0 and is_pressed_volume == False:
-      is_pressed_volume = True
-      volume_mode = not volume_mode
-      set_volume(volume_mode)
-      time.sleep(0.1)
-  if botao_volume.value() == 1 and is_pressed_volume == True:
-      is_pressed_volume = False
-      time.sleep(0.1)
-
-
-
-  #Posicao
-  if botao_posicao.value() == 0 and is_pressed_position == False:
-      is_pressed_position = True
-      position_mode = not position_mode
-      set_position(position_mode)
-      time.sleep(0.1)
-  if botao_posicao.value() == 1 and is_pressed_position == True:
-      is_pressed_position = False
-      time.sleep(0.1)
+  
+  volume.rotation()
 
 
 
@@ -128,6 +116,4 @@ while True:
 
 
 
-
-
-
+#arrumar trava de botao para apenas um botao por vez// clocar memororia para volume e bloquear mudança de volume sem estar no modo de volume(agora ele continuar contando mesmo sem estar e add quando entra )
